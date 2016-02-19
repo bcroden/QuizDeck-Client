9fb06b5910e909b7c5356e827a67585373a4629f
Brandon Roden <brandoncroden@gmail.com>
2016-02-19 10:23:08 -0600

    Don't reference a service in a config block...

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -3,14 +3,17 @@
 
     angular
         .module('app')
-        .config(config);
+        .config(config)
+        .run(run);
     
-    function config($locationProvider, routeAuthService) {
+    function config($locationProvider) {
         $locationProvider.html5Mode({
             enabled: true,
             requireBase: false
         });
-        
+    }
+    
+    function run(routeAuthService) {
         routeAuthService.init();
     }
 })();
