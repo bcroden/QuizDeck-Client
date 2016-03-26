aa5bf201dbf29a1b17772a6082a399fcb0da791b
Brandon Roden <brandoncroden@gmail.com>
2016-03-26 11:10:00 -0500

    Wakeup server on load

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -13,7 +13,15 @@
         });
     }
     
-    function run(routeAuthService) {
+    function run(routeAuthService, $http, serverUrl) {
         routeAuthService.init();
+        
+        // The server apparently returns plaintext... this is a workaround.
+        // TODO: Yell at server team to make it return json.
+        $http.get(serverUrl+'/rest/nonsecure/server/wakeup/wakup_server', {
+            transformResponse: [function (data) {
+                return data;
+            }]
+        });
     }
 })();
