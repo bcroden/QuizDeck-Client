4404afd6ed6c858ef804721743092bf47da8001f
rxm033 <rxm033@email.uark.edu>
2016-04-12 14:07:13 -0500

    Update qd-quiz-management.component.js
    ADDED AN S TO HTTP

--- src/app/routes/quiz-management/qd-quiz-management.component.js
+++ src/app/routes/quiz-management/qd-quiz-management.component.js
@@ -17,7 +17,7 @@
         
          var dataFromServer = {};
          var listOfData = {};
-         $http.get('http://quizdeckserver.herokuapp.com/rest/secure/quiz/searchBySelf').then(function(responce) {
+         $http.get('https://quizdeckserver.herokuapp.com/rest/secure/quiz/searchBySelf').then(function(responce) {
              dataFromServer = responce.data;
              dataFromServer.forEach(function(quiz){         
                 quiz.categories.forEach(function(category){
