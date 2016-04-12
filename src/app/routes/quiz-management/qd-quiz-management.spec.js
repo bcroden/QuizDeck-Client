7f1a7393732725e0c211e2f1f0af657c972bfa5c
Ryan <rxm033@email.uark.edu>
2016-04-11 21:37:57 -0500

    Rearranged the navbar, added enter functionality to all search buttons, removed some junk code, completely implemented quiz-management html/js, reworked quiz-magement spec.js but haven't finished it.

--- src/app/routes/quiz-management/qd-quiz-management.spec.js
+++ src/app/routes/quiz-management/qd-quiz-management.spec.js
@@ -1,19 +1,72 @@
 'use strict';
 
-ngDescribe({
-    name: 'Quiz-Management',
-    module: 'app',
-    tests: function ($componentController, $rootScope) {
-        var $ctrl;
-        
-        beforeEach(function() {
-            $ctrl = $componentController('qdQuizManagement', {
-                $scope: $rootScope.$new()
-            });
+describe('Quiz-Management Page', function(){
+    var $ctrl;
+
+    var $rootScope;
+    var $location;
+    var $q;
+    
+      var authService;
+    
+     beforeEach(angular.mock.module('app'));
+     
+     beforeEach(angular.mock.inject(function(_$injector_) {
+        var $injector = _$injector_;
+        $rootScope = $injector.get('$rootScope');
+        $location = $injector.get('$location');
+        var $componentController = $injector.get('$componentController');
+        $q = $injector.get('$q');
+
+        authService = $injector.get('authService');
+
+        $ctrl = $componentController('qdQuizManagement', {
+            $scope: $rootScope.$new()
         });
-        
-        it('controller is defined', function() {
-            expect($ctrl).toBeDefined();
-        });
-    }
+        spyOn($location, 'path');
+    }));
+    
+     it('controller is defined', function() {
+        expect($ctrl).toBeDefined();
+    });
+
+    // describe('searchWithFilter()', function(){
+    //     it('sets a list of quizzes', function(){
+    //         spyOn(authService, 'searchWithFilter').and.callFake(function() {
+    //             return $q.when(true);
+    //         });
+            
+    //         $ctrl.searchWithFilter(
+    //             listOfData["string"].push([
+    //             {
+    //                 "categories": [
+    //                 "string"
+    //                 ],
+    //                 "id": "string",
+    //                 "labels": [
+    //                 "string"
+    //                 ],
+    //                 "owner": "string",
+    //                 "publicAvailable": true,
+    //                 "questions": [
+    //                 {
+    //                     "answers": [
+    //                     {
+    //                         "content": "string",
+    //                         "id": "string"
+    //                     }
+    //                     ],
+    //                     "correctAnswerID": "string",
+    //                     "question": "string",
+    //                     "questionFormat": "string",
+    //                     "questionNum": 0
+    //                 }
+    //                 ],
+    //                 "title": "string"
+    //             }
+    //             ])
+    //         );
+    //         expect($ctrl.listOfData).toBeDefined();
+    //     })
+    // })
 });
