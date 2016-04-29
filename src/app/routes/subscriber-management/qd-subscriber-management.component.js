17a062aeac4847ef79373b47c8aa4fee7881e9d6
Ryan <rxm033@email.uark.edu>
2016-04-28 23:17:00 -0500

    Implemented the subscription management page. Added chips to show if you are subscribed or not and for unsubscribe. Also made calls to the database to get current subscriptions/make a new subscription/unsubscribe to a current subscription

--- src/app/routes/subscriber-management/qd-subscriber-management.component.js
+++ src/app/routes/subscriber-management/qd-subscriber-management.component.js
@@ -8,50 +8,106 @@
             templateUrl: 'app/routes/subscriber-management/qd-subscriber-management.html'
         });
         
-      var dataFromServer = {};
-      var listOfNames = {};
+      var listOfNames;
+      var subscribers;
+      var users;
 
     /* @ngInject */
     function Controller($location, $http) {
 
         var vm = this;
         this.getNames = getNames;
-        this.onSubscribe = onSubscribe;
         
-        vm.userSearch = userSearch;
+                vm.userSearch = userSearch;
+        vm.onSubscribe = onSubscribe;
+        vm.onUnsubscribe = onUnsubscribe;
 
+        function getNames(){            
+            return listOfNames;
+        }
+        
         function userSearch() {
             listOfNames = [];
+            subscribers = [];
+            users = [];
+            
             
             if(vm.waiting)
                 return;
                 
             vm.waiting = true;
             
+
             if(this.userName){
-               $http.get("https://quizdeckserver.herokuapp.com/rest/secure/user/findUser/" + vm.userName).then(function(response){
-                   vm.waiting = false;
-                   dataFromServer = response.data
-                   dataFromServer.forEach(function(name){
-                       listOfNames.push(name.userName);
-                                      console.log(listOfNames[0]);
+               $http.get("https://quizdeckserver.herokuapp.com/rest/secure/user/findUser/" + vm.userName).then(function(foundUsers){
+                   $http.get('https://quizdeckserver.herokuapp.com/rest/secure/user/getSubscriptions/').then(function(foundSubscriptions){
+                         vm.waiting = false;
+                         
+                        subscribers = foundSubscriptions.data;
+                        users = foundUsers.data;
+
+                        users.forEach(function(name){  
+                            subscribers.forEach(function(subscribedUser){
+                                if(name.userName === subscribedUser){
+                                    name.subscribed = true;
+                                }else if(name.subscribed != true){
+                                    name.subscribed = false;
+                                }
+                            });
+                            listOfNames.push(name);
+                        });
+                        
                    });
                })
                .catch(function(){
                    vm.waiting = false;
+                   alert('The subscription failed');
                });
             }
                 
         }
         
-        function getNames(){            
-            return listOfNames;
+        function onUnsubscribe(name){
+             if(vm.waiting)
+                return;
+                
+            vm.waiting = true;
+            
+            $http.put('https://quizdeckserver.herokuapp.com/rest/secure/user/unSubscribe/' + name).then(function(response){
+                
+                vm.waiting = false;
+                
+                if(response.statusText === "OK"){
+                    vm.userSearch();
+                }else{
+                    
+                }
+            })
+            .catch(function(){
+                vm.waiting = false;
+            })
         }
         
         function onSubscribe(name){
-            $http.get("https://quizdeckserver.herokuapp.com/rest/secure/user/subcribe/ryan").then(function(response){
-               console.log(response); 
-            });
+            if(vm.waiting)
+                return;
+                
+            vm.waiting = true;
+            
+            $http.put('https://quizdeckserver.herokuapp.com/rest/secure/user/subscribe/' + name).then(function(response){
+                
+                vm.waiting = false;
+                
+                if(response.statusText === "OK"){
+                    vm.userSearch();
+                }else{
+                    
+                }
+            })
+            .catch(function(){
+                vm.waiting = false;
+            })
         }
-     }
+
+    }
 })();
